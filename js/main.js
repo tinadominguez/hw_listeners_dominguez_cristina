70fe87cf5e851c8844fdfa16a57fdaf280497acd
Tina Dominguez <tina@tinadominguez.com>
2015-11-06 22:10:22 -0500

    paragraph event listener
    index and js updated

--- js/main.js
+++ js/main.js
@@ -1,4 +1,17 @@
+//change header color upon clicking 
+
 var pageHeading = document.getElementbyId('pageHeading');
-pageHeading.addEventListner('click', function(){
+    pageHeading.addEventListner('click', function(){
     page.Heading.className = 'blue';
     });
+
+//make the paragraph go from purple to black for five seconds on a mouseover
+
+par.addEventListener("mouseover", function() {   
+    par.target.style.color = "purple";
+    setTimeout(function() {
+        par.target.style.color = "black";
+        }, 5000);
+        }, false);  
+  });
+
