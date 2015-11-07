ef571c6c9925ddc0b95c4e499c06b3d2f1e56dbf
Tina Dominguez <tina@tinadominguez.com>
2015-11-06 20:30:54 -0500

    listener
    first event listener

--- README.md
+++ README.md
@@ -1,4 +1,7 @@
 # URL
 
 # Resources
-*
+* http://www.lynda.com/JavaScript-tutorials/Creating-DOM-elements/81266/87541-4.html
+* http://www.w3schools.com/js/js_htmldom_eventlistener.asp
+* https://developer.mozilla.org/en-US/docs/Web/Events
+* "JavaScript: A Beginner's Guide, Fourth Edition" by John Pollock 
---
+++ js/main.js
@@ -0,0 +1,9 @@
+var first = document.getElementById("header");{
+    first.addEventListener("click", function)
+    console.log("You clicked me");
+    var paragraph = document.createElement("p");
+    paragraph.innerHTML = ("This is click number x");
+    var element = document.body;
+    element.appendChild(paragraph);
+}
+
