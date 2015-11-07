372125d8a5376349a2e4182d0325e18d3dd9bed7
Tina Dominguez <tina@tinadominguez.com>
2015-11-06 22:25:32 -0500

    last listener
    new page

--- js/main.js
+++ js/main.js
@@ -1,17 +1,30 @@
 //change header color upon clicking 
 
 var pageHeading = document.getElementbyId('pageHeading');
-    pageHeading.addEventListner('click', function(){
-    page.Heading.className = 'blue';
+        pageHeading.addEventListner('click', function(){
+        page.Heading.className = 'blue';
     });
 
 //make the paragraph go from purple to black for five seconds on a mouseover
 
 par.addEventListener("mouseover", function() {   
     par.target.style.color = "purple";
+    
     setTimeout(function() {
         par.target.style.color = "black";
         }, 5000);
         }, false);  
   });
+
+//new page
+
+var new_page = "http://http://www.starwars.com/";
+var a1 = document.getElementById("a1");
+a1.addEventListener("click", function(){
+    window.location = new_page;
+}, false);
+
+
+
+
 
